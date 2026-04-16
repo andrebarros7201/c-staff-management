09703378009b67093592bdd47fac1f09cb5a1069
Andre Barros <andrebarros7201@gmail.com>
2026-04-16 21:28:34 +0100

    fix: add missing menu options

--- src/menu.c
+++ src/menu.c
@@ -1,18 +1,22 @@
 #include "menu.h"
+#include "operations.h"
 #include <stddef.h>
 #include <stdio.h>
 
 void menu() {
     int option;
+    MembersList membersList = {NULL, NULL, 0};
 
     do {
         printf("\nMENU");
         printf("\n[0] Exit");
         printf("\n[1] See All Staff Members");
-        printf("\n[2] Add Staff Member");
-        printf("\n[3] Remove Staff Member");
-        printf("\n[4] Save");
-        printf("\n[5] Load");
+        printf("\n[2] See All Staff Members");
+        printf("\n[3] Add Staff Member");
+        printf("\n[4] Update Staff Member");
+        printf("\n[5] Remove Staff Member");
+        printf("\n[6] Save");
+        printf("\n[7] Load");
         printf("\nOption: ");
 
         scanf_s("%d", &option);
@@ -22,9 +26,40 @@ void menu() {
             printf("Program exited");
             break;
 
+        case 1:
+            displayAllMembers(&membersList);
+            break;
+
+        case 2:
+            int id;
+            printf("ID: ");
+            scanf_s("%d", &id);
+            displayMember(&membersList, id);
+            break;
+
+        case 3:
+            addMember();
+            break;
+
+        case 4:
+            updateMember();
+            break;
+
+        case 5:
+            removeMember();
+            break;
+
+        case 6:
+            saveData();
+            break;
+
+        case 7:
+            loadData();
+            break;
+
         default:
-            printf("Invalid choice");
+            printf("Invalid option");
             break;
         }
-    } while (option < 0 || option > 5);
+    } while (option < 0 || option > 7);
 }
